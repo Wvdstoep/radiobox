f1ac356e7591808684aa42db0e0b23c6d1edc7e2
Wvdstoep <93021284+Wvdstoep@users.noreply.github.com>
2024-08-23 16:00:46 +0200

    Update run.py

--- RadioBoxPayment/run.py
+++ RadioBoxPayment/run.py
@@ -1,12 +1,8 @@
+# app.py or another file where you define your WSGI callable
+
 from app import create_app
 
 app = create_app()
 
-
-def start_app():
-    return app
-
-
 if __name__ == '__main__':
-    app = start_app()
     app.run(host='0.0.0.0', port=5001, debug=True)
